7ff880e80993fe8d8d0e31cb0115f04208dc4da4
Kang-in Raon Kim <is.raonism@gmail.com>
2014-07-17 03:56:08 +0900

    get dom which is target of mouse click event and get info of the dom.

--- controller/libs/getDom.js
+++ controller/libs/getDom.js
@@ -1,3 +1,14 @@
 function getDom() {
+  $(document).mouseup(function(event) {
+    var target = jQuery(event.target);
+
+    var offset = {
+      top: returnIntVal(target.offset().top) + returnIntVal(target.css('padding-top')),
+      left: returnIntVal(target.offset().left) + returnIntVal(target.css('padding-left')),
+      width: returnIntVal(target.innerWidth()) - returnIntVal(target.css('padding-left')) - returnIntVal(target.css('padding-right')),
+      height: returnIntVal(target.innerHeight()) - returnIntVal(target.css('padding-top')) - returnIntVal(target.css('padding-bottom')),
+    }
+  });
+
   return;
 }
